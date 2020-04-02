4586e6324fbe287f24ea00a788ba8ea9f3bd4d49
Nick Holke <nickholke@gmail.com>
2020-04-02 08:26:42 -0700

    brute force

---
+++ permutationPalindrome.js
@@ -0,0 +1,33 @@
+function hasPalindromePermutation(theString) {
+
+  // Check if any permutation of the input is a palindrome
+  let permutations = new Set();
+  const innerRec = (str, chars) => {
+    if (str.length === theString.length) {
+      if (isPalindrome(str)) {
+        permutations.add(str);
+      }
+      return;
+    }
+    
+    for (let i = 0; i < chars.length; i++) {
+      let newChars = chars.slice(0,i).concat(chars.slice(i+1));
+      innerRec(str + chars[i], newChars);
+    }
+  }
+  
+  innerRec('', theString);
+  if (permutations.size) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isPalindrome(str) {
+  for (let i = 0, j = str.length - 1; i < str.length / 2; i++, j--) {
+    if (str[i] !== str[j]) return false;
+  }
+  
+  return true;
+}
