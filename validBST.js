0d0d2fc9124c226969cb95ec920a08603e1c5041
Nick Holke <nickholke@gmail.com>
2020-04-18 14:02:36 -0700

    DFS

--- validBST.js
+++ validBST.js
@@ -33,3 +33,39 @@ function isBinarySearchTree(treeRoot) {
 
   return true;
 }
+
+function isBinarySearchTree(treeRoot) {
+
+  // Determine if the tree is a valid binary search tree
+  const q = [{
+    node: treeRoot,
+    lowerBound: -Infinity,
+    upperBound: Infinity
+  }];
+  
+  while (q.length) {
+    const {node, lowerBound, upperBound} = q.pop();
+    
+    if (node.value <= lowerBound || node.value >= upperBound) {
+      return false;
+    }
+    
+    if (node.left) {
+      q.push({
+        node: node.left,
+        lowerBound,
+        upperBound: node.value
+      });
+    }
+    
+    if (node.right) {
+      q.push({
+        node: node.right,
+        lowerBound: node.value,
+        upperBound
+      });
+    }
+  }
+
+  return true;
+}
